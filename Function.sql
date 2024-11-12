c6626f88084f32ace89c3df9d895412956d6edcf
LavenantBal_Gilberto. <147429615+MaruchanDeRes@users.noreply.github.com>
2024-11-11 21:20:48 -0800

    Update Function.sql

--- Function.sql
+++ Function.sql
@@ -1,1 +1,48 @@
--- Aqui van las funciones 
+--Primera Function--
+CREATE FUNCTION dbo.udfEstadoGrupo(@Activo BIT)
+RETURNS NVARCHAR(20)
+AS
+BEGIN
+    DECLARE @Estado NVARCHAR(20);
+    
+    SET @Estado = CASE 
+        WHEN @Activo = 1 THEN 'Activo'
+        WHEN @Activo = 0 THEN 'Inactivo'
+        ELSE '**Desconocido**'
+    END;
+    
+    RETURN @Estado;
+END;
+GO
+-- Segunda Fuction --
+CREATE FUNCTION dbo.udfDescripcionCancelacion(@TipoCancelacion INT)
+RETURNS NVARCHAR(50)
+AS
+BEGIN
+    DECLARE @Descripcion NVARCHAR(50);
+
+    SET @Descripcion = CASE 
+        WHEN @TipoCancelacion = 1 THEN 'Motivo personal'
+        WHEN @TipoCancelacion = 2 THEN 'Problema técnico'
+        WHEN @TipoCancelacion = 3 THEN 'Clima adverso'
+        WHEN @TipoCancelacion = 4 THEN 'Reunión urgente'
+        ELSE 'Otro'
+    END;
+
+    RETURN @Descripcion;
+END;
+GO
+--tercera fuction--
+CREATE FUNCTION dbo.udfNombreProfesor(@IdProfesor INT)
+RETURNS NVARCHAR(100)
+AS
+BEGIN
+    DECLARE @Nombre NVARCHAR(100);
+
+    SELECT @Nombre = nombre
+    FROM profesores
+    WHERE id_profesor = @IdProfesor;
+
+    RETURN ISNULL(@Nombre, '**No encontrado**');
+END;
+GO
